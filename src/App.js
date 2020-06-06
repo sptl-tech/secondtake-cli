9f69d80868b70c2bfb6abf5cc284d956b82ea235
sahilp-tech <patelsahil395@gmail.com>
2020-06-06 15:10:27 -0700

    Added take card details and styled navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import{BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+//Material UI 
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+
+//components
+import Navbar from './components/Navbar';
 
 //Pages 
 import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
-//components 
-import Navbar from './components/Navbar';
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ff8a65',
+      light: '#ffb93',
+      dark: '#c75b39',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#fff'
+    }
+  },
+  typography:{
+    useNextVariants: true
+  }
+});
 
-function App() {
-  return (
-    <div className="App">
-      <Router> 
-      <Navbar />
-        <div className = "container">
-          <Switch>
-            <Route exact path = "/" component ={home}/> 
-            <Route exact path = "/login" component ={login}/>
-            <Route exact path = "/signup" component ={signup}/>
-          </Switch>
-        </div>
-      </Router>
+class App extends Component{
+    render(){
+      return(
+        <MuiThemeProvider theme ={theme} >
+            <div className="App">
+            <Router> 
+            <Navbar />
+              <div className = "container">
+            <Switch>
+              <Route exact path = "/" component ={home}/> 
+              <Route exact path = "/login" component ={login}/>
+              <Route exact path = "/signup" component ={signup}/>
+            </Switch>
+            </div>
+            </Router>
       
-    </div>
-  );
+          </div>
+          </MuiThemeProvider>
+      )
+          }
+    
+  
 }
 
 export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
-
-export class home extends Component {
+import axios from 'axios'
+import Grid  from '@material-ui/core/Grid'
+import Take from '../components/Take'
+class home extends Component {
+    state ={
+        takes: null
+    }
+    componentDidMount(){ //send request to server to retreive our information using axios
+        axios.get('/takes')
+            .then( res =>{
+                console.log(res.data)
+                this.setState({
+                    takes: res.data
+                })
+            })
+            .catch(err => console.log(err));
+    }
     render() {
+        let recentTakesMarkup = this.state.takes?(//want to check if state of takes is null; if it is not, then it is still loading the takes
+            this.state.takes.map(take => <Take take ={take}/>)
+        )  : <p>Loading..</p>
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
-        )
+            <Grid container spacing ={2}>
+                <Grid item sm={8} xs={12}> 
+                    {recentTakesMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile..</p>
+                </Grid>
+            </Grid>
+        );
     }
 }
 
